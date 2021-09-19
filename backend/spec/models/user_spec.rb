b6319da6688750e5208279bf4c4cb8b701abd8a8
trunkman <i.trunkman82@gmail.com>
2021-09-19 08:18:48 +0000

    name, emailの検証

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -2,18 +2,69 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  before do 
-    @user = User.new(name: "Example User", email: "user@example.com")
-  end
+  let(:user) { User.new(name: "Example User", email: "user@example.com") }
 
   it "ユーザーが存在している確認" do
-    @user.valid?
-    expect(@user).to be_valid
+    user.valid?
+    expect(user).to be_valid
   end
 
-  it "nameが空であってはならない" do
-    @user.name = nil
-    expect(@user).not_to be_valid    
+  describe "nameのテスト" do
+    
+    it "nameが空であってはならない" do
+      user.name = "  "
+      expect(user).not_to be_valid    
+    end
+    
+    it "nameが長すぎるべきではない" do
+      user.name = "a" * 51
+      expect(user).not_to be_valid    
+    end
+  end
+
+  describe "emailのテスト" do
+
+    it "emailが空であってはならない" do
+      user.email = "  "
+      expect(user).not_to be_valid    
+    end
+    
+    it "emailが長すぎるべきではない" do
+      user.email = "a" * 244 +"@example.com"
+      expect(user).not_to be_valid    
+    end
+  
+    it "正規表現のemailは許容される" do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                             first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        expect(user).to be_valid    
+      end
+    end
+  
+    it "正規表現でないemailは弾かれる" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                          foo@bar_baz.com foo@bar+baz.com]
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user).not_to be_valid    
+      end
+    end
+
+    it "emailは一意性である" do
+      dupulicate_user = user.dup
+      user.save
+      expect(dupulicate_user).not_to be_valid 
+    end
+
+    it "emailは小文字でsaveされる" do
+      mixed_case_email = "Foo@ExAMple.Com"
+      user.email = mixed_case_email
+      user.save
+      expect(mixed_case_email.downcase).to eq user.reload.email 
+    end
+
   end
   
 end
