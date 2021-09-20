3918736f81fe5d0d5eb7509bb4811ed409ba8714
trunkman <i.trunkman82@gmail.com>
2021-09-20 23:16:57 +0000

    User controllerアクションを追加した

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,40 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      # getメソッド
       def index
         @user = User.all
         render json: { users: @user }, status: :ok
       end
 
-      def new; end
+      def show
+        @user = User.find(params[:id]) 
+        render json: { users: @user }, status: :ok
+      end
+
+      def new
+        @user = User.new
+        render json: { users: @user }, status: :ok
+      end
+
+      # postメソッド
+      def create
+        @user = User.new(paramas[user_params])
+        if @user.save
+          flash[:success] = "ユーザーが登録されました"
+          redirect_to @user
+        else
+          render 'new'  
+        end
+
+
+      private
+        
+      # Strong Parameters
+        def user_params
+          params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+      
     end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/home', to: 'users#index'
+
+      get '/signup' to: 'users#new'
       resources :users
     end
   end
