05e0760d6a275cb2c458c59058c919adb5590cd4
trunkman <i.trunkman82@gmail.com>
2021-11-24 18:02:11 +0900

    情報を追加した

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name: 'Admin User',
              password: 'foobar',
              password_confirmation: 'foobar',
              ideal_sleeping_hours: 7.00,
-             profile: 'わたしはこのアプリの管理者です。以前、慢性的な睡眠不足に悩ませておりましたが、睡眠方法を改善し、よく眠るようにより、Quality of Lifeが劇的に向上しました。皆さま方の睡眠向上のお役に立てればと思い、このアプリを制作しました。是非活用してみてください'
+             profile: 'わたしはこのアプリの管理者です。よく眠るようにより、Quality of Lifeが劇的に向上しましたので、睡眠向上をサポートするアプリを制作しました。是非活用してみてください'
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
@@ -14,18 +14,20 @@ User.create!(name: 'Admin User',
 29.times do |n|
   name  = "#{n + 1}君"
   email = "example-#{n + 1}@example.com"
+  profile = "#{n + 1}君のプロフィールとなります。#{n + 1}の経歴、趣味、特技、好きな食べ物などが記載されます。"
   User.create!(name: name,
                email: email,
                password: 'foobar',
                password_confirmation: 'foobar',
                ideal_sleeping_hours: 7.00,
+               profile: profile,
                activated: true,
                activated_at: Time.zone.now)
 end
 
 # ユーザーの一部を対象にマイクロポストを生成する
 users = User.order(:created_at).take(6)
-20.times do |n|
+10.times do |n|
   content = "#{n}番目の投稿：ここに投稿が記載されていきます。"
   users.each { |user| user.microposts.create!(content: content) }
 end
