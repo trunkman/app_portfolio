8a5900516a3ebe29ae82feccd34912ed707a02fb
trunkman <i.trunkman82@gmail.com>
2021-11-24 12:25:01 +0900

    ファイルを作成した

---
+++ frontend/src/reducer/TimelineReducer.jsx
@@ -0,0 +1,47 @@
+export const profileInitialState = {
+  fetchState: 'initial',
+  reRender: false,
+  user: {},
+  followingIds: [],
+  followersIds: [],
+  microposts: [],
+  likedMicroposts: [],
+  comments: [],
+  commentedMicroposts: [],
+}
+
+export const profileReducer = (state, action) => {
+  switch (action.type) {
+    case 'fetching':
+      return {
+        ...state,
+        fetchState: 'loading',
+      };
+    case 'fetchSuccessProfile':
+      return {
+        ...state,
+        fetchState: 'ok',
+        reRender: false,
+        user: action.payload.user,
+        followingIds: action.payload.followingIds,
+        followersIds: action.payload.followersIds,
+      };
+    case 'fetchSuccessMicropost':
+      return {
+        ...state,
+        fetchState: 'ok',
+        reRender: false,
+        microposts: action.payload.microposts,
+        likedMicroposts: action.payload.likedMicroposts,
+        comments: action.payload.comments,
+        commentedMicroposts: action.payload.commentedMicroposts,
+      };
+    case 'reRender':
+      return {
+        ...state,
+        reRender: true,
+      };
+    default:
+      throw new Error();
+  }
+}
